216295ec182fe70c29ed4f8e71fcf84c183483c2
Martin Borho <martin@borho.net>
2016-11-07 17:00:34 +0100

    Initial commit

---
+++ tests/test_converter.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 dpa-infocom GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import asynctest
+import os.path
+from livebridge_scribblelive import LiveblogScribbleliveConverter
+from tests import load_json
+
+class LiveblogScribbleliveConverterTest(asynctest.TestCase):
+
+    def setUp(self):
+        self.converter = LiveblogScribbleliveConverter()
+
+    async def test_simple_conversion(self):
+        post = load_json('post_to_convert.json')
+        res, images = await self.converter.convert(post)
+        assert len(res) >= 1
+        assert res == """<p><b>Text</b> mit ein parr <i>Formatierungen</i>. Und einem <a href="http://dpa.de">Link</a>. Und weiterer <s>Text</s>.<br></p><div><img src="http://newslab-liveblog-demo.s3-eu-central-1.amazonaws.com/aa7c892f1b1b7df17f635106e27c55d86a5c5b6144bebe2490f4ce14be671dd7" /><br>Gähn <i>(Mich)</i></div><p>Listen:<br><br><br> • Eins<br> • Zwei<br> • Drei<br><br><br> • u1<br> • u2<br> • u3<br></p><blockquote>Mit dem Wissen wächst der Zweifel.<br><br> • <i>Johann Wolfgang von Goethe</i></blockquote><p>Nochmal <i><b>abschließender</b></i> Text.</p><div id="_nnc0gyzwv">\n     <blockquote class="twitter-tweet">\n         <p>\nNeue Trendsportart? Der Kick auf dem Brett - Drohnen-Surfen! https://t.co/DjyYgHkYJ3 @dpa-NewsBlog | @noz_de\n         </p>&mdash; \n         dpa·live on Twitter (@dpa_live)\n         <a href="https://twitter.com/dpa_live/status/775991579676909568">https://twitter.com/dpa_live/status/775991579676909568</a>\n     </blockquote>\n</div>"""
+        await self.converter.remove_images(images)
+
+    async def test_simple_conversion_failing(self):
+        post = load_json('post_to_convert.json')
+        # unknown embed
+        post["groups"][1]["refs"] = [{"item": {"item_type" : "testtype"}, "type": "test"}]
+        res, images = await self.converter.convert(post)
+        assert res == ""
+        assert images == []
+
+        # let it fail with catched exception
+        del post["groups"][1]["refs"]
+        res, images = await self.converter.convert(post)
+        assert res == ""
+        assert images == []
+
+    async def test_convert_image(self):
+        post = load_json('post_to_convert.json')
+        self.converter._download_image = asynctest.CoroutineMock(return_value="/tmp/foobaz")
+        img_item = post["groups"][1]["refs"][1]
+        content, tmp_path = await self.converter._convert_image(img_item)
+        assert tmp_path == "/tmp/foobaz"
+        assert content == "<br>Gähn <i>(Mich)</i><br> "
+        self.converter._download_image.assert_called_once_with({
+            'height': 1080,
+            'media': '7966946203766696aed8d067b04972b3a8f695aac885b51675d4117b388c1454',
+            'href': 'http://newslab-liveblog-demo.s3-eu-central-1.amazonaws.com/7966946203766696aed8d067'+\
+                    'b04972b3a8f695aac885b51675d4117b388c1454',
+            'width': 1621,
+            'mimetype': 'image/jpeg'})
+
+    async def test_convert_image_empty_desc_credit(self):
+        post = load_json('post_to_convert.json')
+        self.converter._download_image = asynctest.CoroutineMock(return_value="/tmp/foobaz")
+        img_item = post["groups"][1]["refs"][1]
+        img_item["item"]["meta"]["caption"] = ""
+        img_item["item"]["meta"]["credit"] = ""
+        content, tmp_path = await self.converter._convert_image(img_item)
+        assert content == " "
+
+    async def test_convert_image_failing(self):
+        post = load_json('post_to_convert.json')
+        self.converter._download_image = asynctest.CoroutineMock(side_effect=Exception)
+        img_item = post["groups"][1]["refs"][1]
+        content, tmp_path = await self.converter._convert_image(img_item)
+        assert tmp_path == None
+        assert content == ""
+
+    async def test_convert_image_inline(self):
+        post = load_json('post_to_convert.json')
+        img_item = post["groups"][1]["refs"][1]
+        content, tmp_path = await self.converter._convert_image_inline(img_item)
+        assert tmp_path == None
+        assert content == '<div><img src="http://newslab-liveblog-demo.s3-eu-central-1.amazonaws.com/aa7c892f1b1b7df17f635106e27c55d86a5c5b6144bebe2490f4ce14be671dd7" /><br>Gähn <i>(Mich)</i></div>'
+
+        img_item["item"]["meta"]["media"]["renditions"]["viewImage"] = {}
+        img_item["item"]["meta"]["caption"] = ""
+        img_item["item"]["meta"]["credit"] = ""
+        content, tmp_path = await self.converter._convert_image_inline(img_item)
+        assert tmp_path == None
+        assert content == " "
+
+    async def test_convert_image_inline_failing(self):
+        content, tmp_path = await self.converter._convert_image_inline({})
+        assert tmp_path == None
+        assert content == ''
+
+    async def test_embed_text(self):
+        item = {"item": {"meta": {
+                    "title": "Titel",
+                    "description": "Beschreibung",
+                    "credit": "Credit",
+                }}}
+        content = await self.converter._convert_embed(item)
+        assert content == "<br><div><strong>Titel</strong></div><p>Beschreibung</p><div><i>Credit</i></div>"
+
+        item = {"item": {"meta": {
+                    "title": "Titel",
+                    "description": "Beschreibung",
+                }}}
+        content = await self.converter._convert_embed(item)
+        assert content == "<br><div><strong>Titel</strong></div><p>Beschreibung</p>"
+
+        item = {"item": {"meta": {
+                    "description": "Beschreibung",
+                    "credit": "Credit",
+                }}}
+        content = await self.converter._convert_embed(item)
+        assert content == "<p>Beschreibung</p><div><i>Credit</i></div>"
+
+        item = {"item": {"meta": {
+                    "description": "Beschreibung",
+                }}}
+        content = await self.converter._convert_embed(item)
+        assert content == "<p>Beschreibung</p>"
+
+        item = {"item": {"meta": {
+                    "credit": "Credit",
+                }}}
+        content = await self.converter._convert_embed(item)
+        assert content == "<div><i>Credit</i></div>"
+
+    async def test_convert_fb_embed(self):
+        embed = '<script type="text/javascript" src="https://connect.facebook.net/en_US/all.js#xfbml=1&status=0&appId=">'
+        embed += '</script><div id="_8dyj37s33"><div class="fb-post" data-width="350" data-href="https://www.facebook.com/'
+        embed += 'DeutschePresseAgenturGmbH/posts/764149090389333"><div class="fb-xfbml-parse-ignore"><a href="https://www'
+        embed += '.facebook.com/DeutschePresseAgenturGmbH/posts/764149090389333">Post</a> by <a href="https://www.facebook'
+        embed += '.com/DeutschePresseAgenturGmbH">DeutschePresseAgenturGmbH</a>.</div></div></div>'
+        embed2 = '<script>  if(window.FB !== undefined) {    window.FB.XFBML.parse(document.getElementById("_8dyj37s33"));  }</script>'
+        item = {"item": {
+            "meta": {
+                "provider_name": "Facebook",
+                "html": embed+embed2
+            }
+        }}
+        content = self.converter._prepare_facebook_embed(item["item"]["meta"]["html"])
+        assert content == embed
+
+    async def test_convert_twitter_embed(self):
+        embed = """<div id="_4f1cm9ovf">
+     <blockquote class="twitter-tweet">
+         <p>
+Bundesliga-Relegation live! Nürnberg vs Frankfurt - die Partie jetzt im LiveTicker http://live.fussball.com/fbcom_fb_mbl/html_php/index_fbcom.html#/live-e852618 ... | @fbcompic.twitter.com/via9YxUG2d
+         </p>&mdash; 
+         dpa·live on Twitter (@dpa_live)
+         <a href="https://twitter.com/dpa_live/status/734812416584744960">https://twitter.com/dpa_live/status/734812416584744960</a>
+     </blockquote>
+</div>"""
+        embed2 = """<script>
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],t = window.twttr || {};
+        if (d.getElementById(id)) return t; js = d.createElement(s);js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs); t._e = [];
+        t.ready = function(f) {t._e.push(f);}; return t;}(document, "script", "twitter-wjs"));
+    window.twttr.ready(function(){
+        window.twttr.widgets.load(document.getElementById("_4f1cm9ovf"));
+    });
+</script>"""
+        item = {"item": {
+            "meta": {
+                "html": embed+embed2
+            }
+        }}
+        content = await self.converter._convert_embed(item)
+        assert content == embed
+
+    async def test_convert_youtube(self):
+        embed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/86jugQW9Y9Y" '
+        embed += 'frameborder="0" allowfullscreen></iframe>'
+        item = {"item": {"meta": {"html": embed}}}
+        res = await self.converter._convert_embed(item)
+        assert res == embed
+
+    async def test_convert_youtube_embedly(self):
+        embed = '<iframe class="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%'
+        embed += '2F%2Fwww.youtube.com%2Fembed%2FxiE5AQHKj_Y%3Ffeature%3Doembed%26rel%3D0&url=http%3A%2F%2'
+        embed += 'Fwww.youtube.com%2Fwatch%3Fv%3DxiE5AQHKj_Y&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxiE5AQH'
+        embed += 'Kj_Y%2Fhqdefault.jpg&key=979cf63d19e24e8cbcfcc7c5063867e2&type=text%2Fhtml&schema=youtube"'
+        embed += ' width="350" height="263" scrolling="no" frameborder="0" allowfullscreen></iframe>'
+        item = {"item": {"meta": {"html": embed}}}
+        res = await self.converter._convert_embed(item)
+        assert res == embed
+
+    #@asynctest.skip("Skipped.")
+    async def test_convert_instagram(self):
+        embed = '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style="width:100%;"> <div style="">'
+        embed += '</div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/BE5F7Jgxwe4/" style="'
+        embed += 'word-wrap:break-word;" target="_blank">This little &#34;beach&#34; at Brooklyn Bridge park offers gorgeous views, or '
+        embed += 'for this little tyke, a place to practice riding a bike. Reported by @travelogged, your #BBCLocalite for #NYC</a></p>'
+        embed += ' <p style="white-space:nowrap;">Ein von BBC Travel (@bbc_travel) gepostetes Foto am <time style="line-height:17px;" '
+        embed += 'datetime="2016-05-02T04:50:18+00:00">1. Mai 2016 um 21:50 Uhr</time></p></div></blockquote>'
+        embed2 = '<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>'
+        item = {"item": {
+            "meta": {
+                "html": embed+embed2
+            }
+        }}
+        content = await self.converter._convert_embed(item)
+        assert content == embed
+
+
+    @asynctest.skip("Skipped.")
+    async def test_convert_meta_html(self):
+        item = {"item": {"meta": {"html": "<div>foo</div>"}}}
+        res = await self.converter._convert_embed(item)
+        assert res == "<div>foo</div>"
+
+    @asynctest.skip("Skipped.")
+    async def test_convert_embed_text(self):
+        item = {"item": {"text": "Foobaz\nFoo", "meta": {}}}
+        res = await self.converter._convert_embed(item)
+        assert res == "Foobaz\nFoo"
+
+    async def test_convert_quote(self):
+        item = {"item": {"meta": {"quote": "Zitat", "credit": "Urheber"}}}
+        res = await self.converter._convert_quote(item)
+        assert res == "<blockquote>Zitat<br><br> • <i>Urheber</i></blockquote>"
+
+    async def test_convert_quote_without_credit(self):
+        item = {"item": {"meta": {"quote": "Zitat"}}}
+        res = await self.converter._convert_quote(item)
+        assert res == "<blockquote>Zitat<br></blockquote>"
+
+    async def test_convert_text(self):
+        text = """
+            <p><ul><li>eins</li><li>zwei</li></ul>
+            <ol><li>drei</li><li>vier</li></ol><p><br></p>
+            <strike>STRIKE</strike>
+            <div><b>bold</b><i>italic</i></div></p>
+        """
+        res = await self.converter._convert_text({"item":{"text": text}})
+        assert res == """<p><br> • eins<br> • zwei<br>
+            <br> • drei<br> • vier<br>
+            <s>STRIKE</s>
+            <div><b>bold</b><i>italic</i></div></p>"""
---
+++ tests/test_target.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 dpa-infocom GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import asynctest
+import aiohttp
+import time
+from livebridge.base import BaseTarget, BasePost, TargetResponse
+from livebridge_scribblelive import ScribbleLiveTarget
+from livebridge_scribblelive.common import ScribbleLiveClient
+from tests import load_json
+
+
+class TestPost(BasePost):
+    pass
+
+class ScribbleLiveTargetTests(asynctest.TestCase):
+
+    def setUp(self):
+        self.api_key = "baz"
+        self.user = "foo"
+        self.password = "bla"
+        self.event_id = 12345
+        self.endpoint= "https://example.com/api"
+        self.endpoint_v1= "https://example2.com/api"
+        self.client= ScribbleLiveTarget(config={
+                        "auth": {"api_key": self.api_key, "user": self.user, "password": self.password},
+                        "event_id": self.event_id, "endpoint": self.endpoint, "endpoint_v1": self.endpoint_v1})
+
+    @asynctest.ignore_loop
+    def test_init(self):
+        assert self.client.type == "scribble"
+        assert self.client.api_key == self.api_key
+        assert self.client.user == self.user
+        assert self.client.password == self.password
+        assert self.client.event_id == self.event_id
+        assert self.client.endpoint == self.endpoint
+        assert self.client.endpoint_v1 == self.endpoint_v1
+        assert issubclass(ScribbleLiveTarget, BaseTarget) == True
+        assert issubclass(ScribbleLiveTarget, ScribbleLiveClient) == True
+        assert isinstance(self.client, BaseTarget) == True
+
+    @asynctest.ignore_loop
+    def test_url_params(self):
+        url = self.client._add_url_params("http://test.com/foo?")
+        assert url == "http://test.com/foo?Token=baz&format=json"
+
+        url = self.client._add_url_params("http://test.com/foo?baz=bla")
+        assert url == "http://test.com/foo?baz=bla&Token=baz&format=json"
+
+        self.client.auth_token = "foo"
+        url = self.client._add_url_params("http://test.com/foo?baz=bla")
+        assert url == "http://test.com/foo?baz=bla&Token=baz&format=json&Auth=foo"
+
+    @asynctest.ignore_loop
+    def test_get_id_target(self):
+        post_data = load_json('post_to_convert.json')
+
+        post = TestPost(post_data)
+        post.target_doc = {"Id": 456}
+        assert self.client.get_id_at_target(post) == 456
+        
+        post.target_doc = {}
+        assert self.client.get_id_at_target(post) == None
+
+    async def test_login_ok(self):
+        api_res = {
+            'Name': 'Martin Borho',
+            'Id': 53620032,
+            'Avatar': '',
+            'Auth': 'YjJmNGpqajBFS05KOGdZQU5kSTFLaHlqR1A0UzlRZm10Z0I3bThXdTl6SmVLNm5PVkl5VTVMQmhhSjZXY0M2NA'
+        }
+        self.client._get =  asynctest.CoroutineMock(return_value=api_res)
+        res = await self.client._login()
+        assert res == api_res["Auth"]
+        assert self.client._get.call_args_list[0][0][0] == 'https://example.com/api/user?'
+        assert type(self.client._get.call_args_list[0][1]["auth"]) == aiohttp.BasicAuth
+
+    async def test_login_not_ok(self):
+        self.client._get =  asynctest.CoroutineMock(return_value={})
+        res = await self.client._login()
+        assert res == False
+
+    async def test_check_login(self):
+        self.client.auth_token = "authed_1"
+        self.client.auth_time = time.time()
+        self.client._login = asynctest.CoroutineMock(return_value="authed_2")
+        res = await self.client._check_login()
+        assert res == "authed_1"
+        assert self.client._login.call_count == 0
+
+        self.client.auth_time = time.time()-3610
+        self.client._login =  asynctest.CoroutineMock(return_value="authed_2")
+        res = await self.client._check_login()
+        assert res == "authed_2"
+        assert self.client._login.call_count == 1
+
+    async def test_post_item(self):
+        api_res = {'Id': 252657529, 'IsStuck': 0, 'PostMeta': {'CreationDate': '1460467186'},
+                   'EventId': 220029, 'LastModified': '/Date(1460467186700+0000)/',
+                   'Created': '/Date(1460467186700+0000)/',
+                   'Creator': {'Id': 53620032, 'Name': 'Martin Borho', 'Avatar': ''},
+                    'IsComment': 0, 'Content': 'Test, mit Ü.', 'Type': 'TEXT', 'IsApproved': 1,
+                    'Source': '', 'IsDeleted': 0}
+        self.client._post =  asynctest.CoroutineMock(return_value=api_res)
+        self.client.auth_time = time.time()-10
+        self.client.auth_token = "foobaz"
+
+        post = asynctest.MagicMock()
+        post.images = []
+        post.content = "Test, mit Ü."
+        resp = await self.client.post_item(post)
+        assert type(resp) == TargetResponse
+        assert resp == api_res
+        self.client._post.assert_called_once_with('https://example.com/api/event/12345?', [], u'Test, mit Ü.')
+
+    async def test_update_item(self):
+        api_res = {'IsComment': 0, 'Source': '',
+                   'Creator': {'Id': 53620032, 'Avatar': '', 'Name': 'Martin Borho'},
+                   'Created': '/Date(1460471421403+0000)/', 'EventId': 220029, 'IsApproved': 1,
+                   'IsDeleted': 0, 'PostMeta': {'ShowEditedBy': 'true', 'CreationDate': '1460471421'},
+                   'Content': 'Test, mit Äh.', 'LastModified': '/Date(1460471759447+0000)/',
+                    'IsStuck': 0, 'Id': 252686461, 'Type': 'TEXT'}
+        self.client._put =  asynctest.CoroutineMock(return_value=api_res)
+        self.client.auth_time = time.time()-10
+        self.client.auth_token = "foobaz"
+
+        post = asynctest.MagicMock()
+        post.target_doc = {"Id": "252686461"}
+        post.images = []
+        post.content = "Test, mit Äh."
+        resp = await self.client.update_item(post)
+        assert type(resp) == TargetResponse
+        assert resp == api_res
+        self.client._put.assert_called_once_with('https://example2.com/api/post/252686461?', u'Test, mit Äh.', [])
+
+    async def test_update_item_failing(self):
+        post = asynctest.MagicMock()
+        self.client.get_id_at_target = lambda x: None
+        self.client._check_login = asynctest.CoroutineMock(return_value=None)
+        res = await self.client.update_item(post)
+        assert res == False
+        assert self.client._check_login.call_count == 0
+
+    async def test_handle_extras(self):
+        post = asynctest.MagicMock()
+        post.target_doc = {"Id": "12345"}
+        post.is_deleted = False
+        self.client._handle_sticky = asynctest.CoroutineMock(side_effect=Exception)
+        # throws Exception when handle_sticky is called, which is right
+        with self.assertRaises(Exception):
+            await self.client.handle_extras(post)
+        self.client._handle_sticky.assert_called_once_with(post)
+
+    async def test_handle_sticky_failing(self):
+        post = asynctest.MagicMock()
+        post.is_known = False
+        post.target_doc = {"Id": "12345"}
+        post.is_sticky = True
+        post.get_existing = lambda: {"sticky": "0"}
+        self.client._stick_item = asynctest.CoroutineMock(side_effect=Exception)
+        res = await self.client._handle_sticky(post)
+        assert res == None
+        self.client._stick_item.assert_called_once_with('12345')
+
+    async def test_handle_stick_item(self):
+        post = asynctest.MagicMock()
+        post.is_known = False
+        post.target_doc = {"Id": "12345"}
+        post.is_sticky = True
+        post.get_existing = lambda: {"sticky": "0"}
+        self.client._stick_item = asynctest.CoroutineMock(return_value={"foo":"baz"})
+        self.client._unstick_item = asynctest.CoroutineMock(side_effect=Exception)
+        res = await self.client._handle_sticky(post)
+        assert res == {"foo":"baz"}
+        self.client._stick_item.assert_called_once_with("12345")
+        assert self.client._unstick_item.call_count == 0
+
+        post.is_sticky = False
+        res = await self.client._handle_sticky(post)
+        assert res == None
+        self.client._stick_item.assert_called_once_with("12345")
+        assert self.client._unstick_item.call_count == 0
+
+    async def test_handle_unstick_item(self):
+        post = asynctest.MagicMock()
+        post.is_known = True
+        post.target_doc = {"Id": "12345"}
+        post.is_sticky = False
+        post.get_existing = lambda: {"sticky": "1"}
+        self.client._unstick_item = asynctest.CoroutineMock(return_value={"foo":"baz"})
+        self.client._stick_item = asynctest.CoroutineMock(side_effect=Exception)
+        res = await self.client._handle_sticky(post)
+        assert res == {"foo":"baz"}
+        self.client._unstick_item.assert_called_once_with("12345")
+        assert self.client._stick_item.call_count == 0
+
+        post.get_existing = lambda: {"sticky": "0"}
+        res = await self.client._handle_sticky(post)
+        assert res == None
+
+    async def test_stick_post(self):
+        api_res = {'IsComment': 0, 'Source': '',
+                   'Creator': {'Id': 53620032, 'Avatar': '', 'Name': 'Martin Borho'},
+                   'Created': '/Date(1460471421403+0000)/', 'EventId': 220029, 'IsApproved': 1,
+                   'IsDeleted': 0, 'PostMeta': {'ShowEditedBy': 'true', 'CreationDate': '1460471421'},
+                   'Content': 'Test, mit Äh.', 'LastModified': '/Date(1460471759447+0000)/',
+                    'IsStuck': 1, 'Id': 252686461, 'Type': 'TEXT'}
+        self.client._get =  asynctest.CoroutineMock(return_value=api_res)
+
+        post_id = 252686461
+        resp = await self.client._stick_item(post_id)
+        assert resp["Id"] == post_id
+        assert resp["IsStuck"] == 1
+        assert self.client._get.call_count == 2
+        self.client._get.assert_called_with('https://example.com/api/post/252686461/stick?')
+
+    async def test_unstick_post(self):
+        api_res = {'IsComment': 0, 'Source': '',
+                   'Creator': {'Id': 53620032, 'Avatar': '', 'Name': 'Martin Borho'},
+                   'Created': '/Date(1460471421403+0000)/', 'EventId': 220029, 'IsApproved': 1,
+                   'IsDeleted': 0, 'PostMeta': {'ShowEditedBy': 'true', 'CreationDate': '1460471421'},
+                   'Content': 'Test, mit Äh.', 'LastModified': '/Date(1460471759447+0000)/',
+                    'IsStuck': 0, 'Id': 252686461, 'Type': 'TEXT'}
+        self.client._get =  asynctest.CoroutineMock(return_value=api_res)
+
+        post_id = 252686461
+        resp = await self.client._unstick_item(post_id)
+        assert resp["Id"] == post_id
+        assert resp["IsStuck"] == 0
+        assert self.client._get.call_count == 2
+        self.client._get.assert_called_with('https://example.com/api/post/252686461/unstick?')
+
+    async def test_delete_item(self):
+        api_res = {'IsStuck': 0, 'Content': 'Test, mit Äh.', 'IsComment': 0, 'EventId': 220029,
+                   'Type': 'TEXT', 'IsApproved': 1, 'LastModified': '/Date(1460472667143+0000)/',
+                   'Source': '', 'Id': 252686461, 'IsDeleted': 1,
+                   'Editor': {'Avatar': '', 'Id': 53620032, 'Name': 'Martin Borho'},
+                   'Creator': {'Avatar': '', 'Id': 53620032, 'Name': 'Martin Borho'},
+                   'Created': '/Date(1460471421403+0000)/',
+                    'PostMeta': {'ShowEditedBy': 'true', 'CreationDate': '1460471421'}}
+        self.client._get =  asynctest.CoroutineMock(return_value=api_res)
+
+        post = asynctest.MagicMock()
+        post.id = 252686461
+        post.target_doc = api_res
+        resp = await self.client.delete_item(post)
+        assert type(resp) == TargetResponse
+        assert resp["Id"] == post.id
+        assert resp["IsDeleted"] == 1
+        assert self.client._get.call_count == 2
+        self.client._get.assert_called_with('https://example.com/api/post/252686461/delete?')
+
+    async def test_delete_item_failing(self):
+        post = asynctest.MagicMock()
+        self.client.get_id_at_target = lambda x: None
+        self.client._check_login = asynctest.CoroutineMock(return_value=None)
+        res = await self.client.delete_item(post)
+        assert res == False
+        assert self.client._check_login.call_count == 0
+
